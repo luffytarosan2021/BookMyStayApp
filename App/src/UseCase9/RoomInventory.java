534446e758ac05eaff8f6567d68c9bee4d175124
luffytarosan2021 <sthadiya567@gmail.com>
2026-03-18 10:54:34 +0530

    Diya Added UseCase9

---
+++ App/src/UseCase9/RoomInventory.java
@@ -0,0 +1,12 @@
+/**
+ * CLASS RoomInventory
+ * Stub for Use Case 9 compilation.
+ */
+public class RoomInventory {
+
+    public boolean hasAvailableRooms(String roomType) {
+        // For testing the validation logic, we will assume rooms are always available.
+        // If you want to test the inventory validation failure, change this to return false.
+        return true;
+    }
+}
